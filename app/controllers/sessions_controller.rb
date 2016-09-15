7cf8aa02afa8e0432202948ff2b696be69ae1f0e
Nicholas <nicholasnkw@gmail.com>
2016-09-15 17:35:09 +0800

    facebook login complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,8 +12,8 @@ class SessionsController < Clearance::SessionsController
         @notice = "Signed in!"
       else
         user = User.create_with_auth_and_hash(authentication,auth_hash)
-        @next = edit_user_path(user)   
-        @notice = "User created - confirm or edit details..."
+        @next = root_url 
+        @notice = "New user created"
       end
       sign_in(user)
       redirect_to @next, :notice => @notice
