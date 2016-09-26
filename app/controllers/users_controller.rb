8aa28c6e6082f412cd3cc54e80c516b7b82ec91c
Nurul Hanisah Kamarul Zaman <HanisahKZ@Nuruls-MacBook-Air.local>
2016-09-26 18:34:32 +0800

    Add CRUD for user and image upload

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,29 @@ class UsersController < Clearance::UsersController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    # @user = User.find(params[:id])
+    @user = current_user
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user 
+    else
+      render 'edit'
+    end
+  end 
+
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :birthday, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :birthday, :email, :password, :photo)
     # params[Clearance.configuration.user_parameter] || Hash.new
   end
 
